957630d55153af5d72f643401958c799618898cc
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-02-01 21:54:15 -0500

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :channel_users
+  has_many :channels, through: :channel_users
+  has_many :messages
 end
